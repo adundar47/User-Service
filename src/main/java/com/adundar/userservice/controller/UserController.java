568887c0c7e881e7adaad7ddb76f90b920751fb9
adundar <adundar@netas.com.tr>
2019-05-15 16:18:28 +0300

    Initial Commit

---
+++ src/main/java/com/adundar/userservice/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.adundar.userservice.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adundar.userservice.exception.AlreadyExistException;
+import com.adundar.userservice.exception.BadRequestException;
+import com.adundar.userservice.exception.NotFoundException;
+import com.adundar.userservice.model.User;
+import com.adundar.userservice.service.UserService;
+
+@RestController
+@RequestMapping("/api/1.0")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/users")
+    public ResponseEntity<?> createUser(@Valid @RequestBody User user) throws AlreadyExistException {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<?> retrieveUser(@PathVariable String userId) throws NotFoundException {
+        return ResponseEntity.ok(userService.retrieveUser(userId));
+    }
+
+    @GetMapping("/users/{name}")
+    public ResponseEntity<?> retrieveUserByName(@PathVariable String name) throws NotFoundException {
+        return ResponseEntity.ok(userService.retrieveUserByName(name));
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable String userId) throws NotFoundException {
+        return ResponseEntity.ok(userService.deleteUser(userId));
+    }
+
+    @PutMapping("/users/{userId}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userId)
+            throws BadRequestException, NotFoundException {
+        return ResponseEntity.ok(userService.updateUser(user, userId));
+
+    }
+}
---
+++ src/main/java/com/adundar/userservice/service/UserService.java
@@ -0,0 +1,84 @@
+package com.adundar.userservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import com.adundar.userservice.bus.producer.MessageProducerService;
+import com.adundar.userservice.exception.AlreadyExistException;
+import com.adundar.userservice.exception.BadRequestException;
+import com.adundar.userservice.exception.NotFoundException;
+import com.adundar.userservice.model.Result;
+import com.adundar.userservice.model.User;
+import com.adundar.userservice.repository.UserRepository;
+import com.adundar.userservice.utils.Utils;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository         userRepository;
+
+    @Autowired
+    MessageProducerService messageProducerService;
+
+    public Result<?> createUser(User user) throws AlreadyExistException {
+        if (userRepository.findByName(user.getName()) != null)
+            throw new AlreadyExistException(Utils.getUserNameAlreadyExistError(user.getName()));
+
+        userRepository.save(user);
+
+        messageProducerService.sendCreateUserEvent(user);
+
+        return Result.success(HttpStatus.CREATED, user);
+    }
+
+    public Result<?> getAllUsers() {
+        return Result.success(HttpStatus.OK, userRepository.findAll());
+    }
+
+    public Result<?> retrieveUser(String userId) throws NotFoundException {
+        User user = userRepository.findOne(userId);
+        if (user != null)
+            throw new NotFoundException(Utils.getUserIdNotFoundError(userId));
+
+        return Result.success(HttpStatus.OK, user);
+    }
+
+    public Result<?> retrieveUserByName(String name) throws NotFoundException {
+        User user = userRepository.findByName(name);
+        if (user == null)
+            throw new NotFoundException(Utils.getUserNameNotFoundError(name));
+
+        return Result.success(HttpStatus.OK, user);
+    }
+
+    public Result<?> deleteUser(String userId) throws NotFoundException {
+        User user = userRepository.findOne(userId);
+        if (user == null)
+            throw new NotFoundException(Utils.getUserIdNotFoundError(userId));
+
+        userRepository.delete(user);
+
+        messageProducerService.sendDeleteUserEvent(userId);
+
+        return Result.success(HttpStatus.OK, user);
+    }
+
+    public Result<?> updateUser(User user, String userId) throws BadRequestException, NotFoundException {
+        User userOld = userRepository.findOne(userId);
+        if (userOld == null)
+            throw new NotFoundException(Utils.getUserIdNotFoundError(userId));
+
+        if (!userOld.getName().equals(user.getName()))
+            throw new BadRequestException(Utils.USER_NAME_CHANGE_ERROR);
+
+        user.setId(userOld.getId());
+
+        User updatedUser = userRepository.save(user);
+
+        messageProducerService.sendUpdateUserEvent(updatedUser);
+
+        return Result.success(HttpStatus.OK, updatedUser);
+    }
+}
