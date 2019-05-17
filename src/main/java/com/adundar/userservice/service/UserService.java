3500c235d9fa47876ef6a5f501ae2b2320cf0418
adundar <adundar@netas.com.tr>
2019-05-17 11:05:17 +0300

    log added.

--- src/main/java/com/adundar/userservice/service/UserService.java
+++ src/main/java/com/adundar/userservice/service/UserService.java
@@ -1,5 +1,7 @@
 package com.adundar.userservice.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,8 @@ import com.adundar.userservice.utils.Utils;
 @Service
 public class UserService {
 
+    private final Logger   LOGGER = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     UserRepository         userRepository;
 
@@ -23,6 +27,9 @@ public class UserService {
     MessageProducerService messageProducerService;
 
     public Result<?> createUser(User user) throws AlreadyExistException {
+
+        LOGGER.debug("[createUser]: User is : {}", user.toString());
+
         if (userRepository.findByName(user.getName()) != null)
             throw new AlreadyExistException(Utils.getUserNameAlreadyExistError(user.getName()));
 
@@ -34,26 +41,37 @@ public class UserService {
     }
 
     public Result<?> getAllUsers() {
+        LOGGER.debug("[getAllUsers]: Get All Users request is processing.");
         return Result.success(HttpStatus.OK, userRepository.findAll());
     }
 
     public Result<?> retrieveUser(String userId) throws NotFoundException {
+        LOGGER.debug("[retrieveUser]: Get User request is processing. userId -> {}", userId);
+
         User user = userRepository.findOne(userId);
         if (user == null)
             throw new NotFoundException(Utils.getUserIdNotFoundError(userId));
+
+        LOGGER.debug("[retrieveUser]: Get User request is processing. user -> {}", user.toString());
 
         return Result.success(HttpStatus.OK, user);
     }
 
     public Result<?> retrieveUserByName(String name) throws NotFoundException {
+        LOGGER.debug("[retrieveUserByName]: Get User request is processing. name -> {}", name);
+
         User user = userRepository.findByName(name);
         if (user == null)
             throw new NotFoundException(Utils.getUserNameNotFoundError(name));
+
+        LOGGER.debug("[retrieveUserByName]: Get User request is processing. user -> {}", user.toString());
 
         return Result.success(HttpStatus.OK, user);
     }
 
     public Result<?> deleteUser(String userId) throws NotFoundException {
+        LOGGER.debug("[deleteUser]: Delete User request is processing. userId -> {}", userId);
+
         User user = userRepository.findOne(userId);
         if (user == null)
             throw new NotFoundException(Utils.getUserIdNotFoundError(userId));
@@ -62,10 +80,14 @@ public class UserService {
 
         messageProducerService.sendDeleteUserEvent(user);
 
+        LOGGER.debug("[deleteUser]: Delete User request is processing. user -> {}", user.toString());
+
         return Result.success(HttpStatus.OK, user);
     }
 
     public Result<?> updateUser(User user, String userId) throws BadRequestException, NotFoundException {
+        LOGGER.debug("[updateUser]: Update User request is processing. userId -> {}", userId);
+
         User userOld = userRepository.findOne(userId);
         if (userOld == null)
             throw new NotFoundException(Utils.getUserIdNotFoundError(userId));
@@ -78,6 +100,8 @@ public class UserService {
         User updatedUser = userRepository.save(user);
 
         messageProducerService.sendUpdateUserEvent(updatedUser);
+
+        LOGGER.debug("[updateUser]: Update User request is processing. user -> {}", updatedUser.toString());
 
         return Result.success(HttpStatus.OK, updatedUser);
     }
